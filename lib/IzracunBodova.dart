199b110d3267b10ac3d47ac5b68e90b440d5d834
Mislav Čupić <mislav.cupic@gmail.com>
2024-05-07 08:48:36 +0200

    slideri

--- lib/IzracunBodova.dart
+++ lib/IzracunBodova.dart
@@ -1,39 +1,31 @@
 import 'package:flutter/material.dart';
 
 class IzracunBodova extends StatefulWidget {
-  const IzracunBodova({super.key});
-
-  // Unutar klase _IzracunBodovaState
-
+  const IzracunBodova({Key? key}) : super(key: key);
 
   @override
   _IzracunBodovaState createState() => _IzracunBodovaState();
 }
 
 class _IzracunBodovaState extends State<IzracunBodova> {
-  // Promijenjene varijable za kalkulaciju u int
-  int opciUspjeh5 = 0;
-  int opciUspjeh6 = 0;
-  int opciUspjeh7 = 0;
-  int opciUspjeh8 = 0;
-  int hrvatskiJezik7 = 0;
-  int matematika7 = 0;
-  int hrvatskiJezik8 = 0;
-  int matematika8 = 0;
-  int straniJezik7 = 0;
-  int straniJezik8 = 0;
-  int kemija7 = 0;
-  int kemija8 = 0;
-  int fizika7 = 0;
-  int fizika8 = 0;
-  int tehnicki7 = 0;
-  int tehnicki8 = 0;
-  int ukupnoBodova = 0;
-
-  // Promijenjene bodovne vrijednosti pragova
-  int bodovniPragElektrotehnicar = 60;
-  int bodovniPragTehnicarZaElektroniku = 60;
-  int bodovniPragTehnicarZaRacunalstvo = 64;
+  // Promijenjene varijable za kalkulaciju u double
+  double opciUspjeh5 = 0;
+  double opciUspjeh6 = 0;
+  double opciUspjeh7 = 0;
+  double opciUspjeh8 = 0;
+  double hrvatskiJezik7 = 0;
+  double matematika7 = 0;
+  double hrvatskiJezik8 = 0;
+  double matematika8 = 0;
+  double straniJezik7 = 0;
+  double straniJezik8 = 0;
+  double kemija7 = 0;
+  double kemija8 = 0;
+  double fizika7 = 0;
+  double fizika8 = 0;
+  double tehnicki7 = 0;
+  double tehnicki8 = 0;
+  double ukupnoBodova = 0;
 
   // Declare variable to store selected smjer
   String selectedSmjer = '';
@@ -43,15 +35,16 @@ class _IzracunBodovaState extends State<IzracunBodova> {
     // Na primjer, možete koristiti switch-case za različite smjerove i vratiti odgovarajući bodovni prag
     switch (smjer) {
       case 'Elektrotehničar':
-        return bodovniPragElektrotehnicar;
+        return 60;
       case 'Tehničar za elektroniku':
-        return bodovniPragTehnicarZaElektroniku;
+        return 60;
       case 'Tehničar za računalstvo':
-        return bodovniPragTehnicarZaRacunalstvo;
+        return 64;
       default:
         return 0; // Defaultni bodovni prag, možete prilagoditi prema potrebi
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,85 +89,91 @@ class _IzracunBodovaState extends State<IzracunBodova> {
                 },
               ),
               const SizedBox(height: 20),
-              // Text inputs for various subjects
-              _buildTextInput('Opći uspjeh 5. razred', opciUspjeh5.toString(), (newValue) {
+              // Slider inputs for various subjects
+              _buildSlider('Opći uspjeh 5. razred', opciUspjeh5, (newValue) {
                 setState(() {
-                  opciUspjeh5 = int.tryParse(newValue) ?? 0;
+                  opciUspjeh5 = newValue;
                 });
               }),
-              _buildTextInput('Opći uspjeh 6. razred', opciUspjeh6.toString(), (newValue) {
+              _buildSlider('Opći uspjeh 6. razred', opciUspjeh6, (newValue) {
                 setState(() {
-                  opciUspjeh6 = int.tryParse(newValue) ?? 0;
+                  opciUspjeh6 = newValue;
                 });
               }),
-              _buildTextInput('Opći uspjeh 7. razred', opciUspjeh7.toString(), (newValue) {
+              _buildSlider('Opći uspjeh 7. razred', opciUspjeh7, (newValue) {
                 setState(() {
-                  opciUspjeh7 = int.tryParse(newValue) ?? 0;
+                  opciUspjeh7 = newValue;
                 });
               }),
-              _buildTextInput('Opći uspjeh 8. razred', opciUspjeh8.toString(), (newValue) {
+              _buildSlider('Opći uspjeh 8. razred', opciUspjeh8, (newValue) {
                 setState(() {
-                  opciUspjeh8 = int.tryParse(newValue) ?? 0;
+                  opciUspjeh8 = newValue;
                 });
               }),
-              _buildTextInput('Hrvatski jezik (7. razred)', hrvatskiJezik7.toString(), (newValue) {
+              _buildSlider(
+                  'Hrvatski jezik (7. razred)', hrvatskiJezik7, (newValue) {
                 setState(() {
-                  hrvatskiJezik7 = int.tryParse(newValue) ?? 0;
+                  hrvatskiJezik7 = newValue;
                 });
               }),
-              _buildTextInput('Matematika (7. razred)', matematika7.toString(), (newValue) {
+              _buildSlider('Matematika (7. razred)', matematika7, (newValue) {
                 setState(() {
-                  matematika7 = int.tryParse(newValue) ?? 0;
+                  matematika7 = newValue;
                 });
               }),
-              _buildTextInput('Hrvatski jezik (8. razred)', hrvatskiJezik8.toString(), (newValue) {
+              _buildSlider(
+                  'Hrvatski jezik (8. razred)', hrvatskiJezik8, (newValue) {
                 setState(() {
-                  hrvatskiJezik8 = int.tryParse(newValue) ?? 0;
+                  hrvatskiJezik8 = newValue;
                 });
               }),
-              _buildTextInput('Matematika (8. razred)', matematika8.toString(), (newValue) {
+              _buildSlider('Matematika (8. razred)', matematika8, (newValue) {
                 setState(() {
-                  matematika8 = int.tryParse(newValue) ?? 0;
+                  matematika8 = newValue;
                 });
               }),
-              _buildTextInput('Strani jezik (7. razred)', straniJezik7.toString(), (newValue) {
+              _buildSlider(
+                  'Strani jezik (7. razred)', straniJezik7, (newValue) {
                 setState(() {
-                  straniJezik7 = int.tryParse(newValue) ?? 0;
+                  straniJezik7 = newValue;
                 });
               }),
-              _buildTextInput('Strani jezik (8. razred)', straniJezik8.toString(), (newValue) {
+              _buildSlider(
+                  'Strani jezik (8. razred)', straniJezik8, (newValue) {
                 setState(() {
-                  straniJezik8 = int.tryParse(newValue) ?? 0;
+                  straniJezik8 = newValue;
                 });
               }),
-              _buildTextInput('Kemija (7. razred)', kemija7.toString(), (newValue) {
+              _buildSlider('Kemija (7. razred)', kemija7, (newValue) {
                 setState(() {
-                  kemija7 = int.tryParse(newValue) ?? 0;
+                  kemija7 = newValue;
                 });
               }),
-              _buildTextInput('Kemija (8. razred)', kemija8.toString(), (newValue) {
+              _buildSlider('Kemija (8. razred)', kemija8, (newValue) {
                 setState(() {
-                  kemija8 = int.tryParse(newValue) ?? 0;
+                  kemija8 = newValue;
                 });
               }),
-              _buildTextInput('Fizika (7. razred)', fizika7.toString(), (newValue) {
+              _buildSlider('Fizika (7. razred)', fizika7, (newValue) {
                 setState(() {
-                  fizika7 = int.tryParse(newValue) ?? 0;
+                  fizika7 = newValue;
                 });
               }),
-              _buildTextInput('Fizika (8. razred)', fizika8.toString(), (newValue) {
+              _buildSlider('Fizika (8. razred)', fizika8, (newValue) {
                 setState(() {
-                  fizika8 = int.tryParse(newValue) ?? 0;
+                  fizika8 = newValue;
                 });
               }),
-              _buildTextInput('Tehnička kultura (7. razred)', tehnicki7.toString(), (newValue) {
+              _buildSlider(
+                  'Tehnička kultura (7. razred)', tehnicki7, (newValue) {
                 setState(() {
-                  tehnicki7 = int.tryParse(newValue) ?? 0;
+                  tehnicki7 = newValue;
                 });
               }),
-              _buildTextInput('Tehnička kultura (8. razred)', tehnicki8.toString(), (newValue) {
+              _buildSlider(
+                  'Tehnička kultura (8. razred)', tehnicki8, (newValue) {
                 setState(() {
-                  tehnicki8 = int.tryParse(newValue) ?? 0;
+                  tehnicki8 = newValue;
                 });
               }),
 
@@ -202,22 +201,24 @@ class _IzracunBodovaState extends State<IzracunBodova> {
                     // Ako nisu sva polja popunjena, prikažite poruku
                     showDialog(
                       context: context,
-                      builder: (context) => AlertDialog(
-                        title: const Text('Upozorenje'),
-                        content: const Text('Molimo ispunite sva polja prije izračuna bodova.'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              print(ukupnoBodova);
-                              Navigator.pop(context);
-                            },
-                            child: const Text('OK'),
+                      builder: (context) =>
+                          AlertDialog(
+                            title: const Text('Upozorenje'),
+                            content: const Text(
+                                'Molimo ispunite sva polja prije izračuna bodova.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  print(ukupnoBodova);
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
                     );
                   } else {
-                    // Ako su sva polja popunjena, izračunajte ukupne bodove
+                    // Ako jesu, izračunajte ukupne bodove
                     ukupnoBodova = opciUspjeh5 +
                         opciUspjeh6 +
                         opciUspjeh7 +
@@ -242,75 +243,88 @@ class _IzracunBodovaState extends State<IzracunBodova> {
 
                       // Provjerite usporedbu ukupnih bodova s bodovnim pragom za odabrani smjer
                       if (ukupnoBodova > bodovniPragSmjera) {
-                        // Ako su ukupni bodovi veći od bodovnog praga, čestitajte
+                        // Ako jesu, čestitajte
                         showDialog(
                           context: context,
-                          builder: (context) => AlertDialog(
-                            title: Text('Čestitamo! Procijenjeni broj bodova je $ukupnoBodova'),
-                            content: const Text('Imate velike šanse za upis našeg smjera.'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'),
+                          builder: (context) =>
+                              AlertDialog(
+                                title: Text(
+                                    'Čestitamo! Procijenjeni broj bodova je $ukupnoBodova'),
+                                content: const Text(
+                                    'Imate velike šanse za upis našeg smjera.'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
                         );
                       } else {
-                        // Ako su ukupni bodovi manji ili jednaki od bodovnog praga, upozorite korisnika
+                        // Ako nisu, upozorite korisnika
                         showDialog(
                           context: context,
-                          builder: (context) => AlertDialog(
-                            title: Text('Vaš procijenjeni broj bodova je $ukupnoBodova'),
-                            content: const Text('Broj bodova koji ste ostvarili nije dovoljan za upis na naš smjer. Nastavite se truditi!'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'),
+                          builder: (context) =>
+                              AlertDialog(
+                                title: Text(
+                                    'Vaš procijenjeni broj bodova je $ukupnoBodova'),
+                                content: const Text(
+                                    'Broj bodova koji ste ostvarili nije dovoljan za upis na odabrani smjer. Nastavite se truditi!'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
                         );
                       }
                     }
                   }
                 },
                 child: const Text('Izračunaj'),
-
-
-
               ),
-        ]
+            ],
           ),
         ),
       ),
     );
   }
 
-  // Function to build text input field
-  Widget _buildTextInput(String labelText, String value, void Function(String) onChanged) {
+  // Function to build slider
+  Widget _buildSlider(String labelText, double value,
+      void Function(double) onChanged) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            flex: 2,
-            child: Text(labelText),
+          Text(labelText),
+          Slider(
+            value: value,
+            min: 0,
+            max: 5,
+            divisions: 5,
+            label: value.toString(),
+            onChanged: onChanged,
           ),
-          const SizedBox(width: 10),
-          Expanded(
-            flex: 1,
-            child: TextFormField(
-              initialValue: value,
-              keyboardType: TextInputType.number,
-              onChanged: onChanged, // Ovdje proslijeđujemo funkciju onChanged koja će se pozvati kada se promijeni vrijednost polja
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('0'),
+              Text('1'),
+              Text('2'),
+              Text('3'),
+              Text('4'),
+              Text('5'),
+            ],
           ),
         ],
       ),
     );
-  }}
+  }
+}
